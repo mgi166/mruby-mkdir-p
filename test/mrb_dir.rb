e58a7030ad2a04e4ecf078112f4ead029d3a4c86
mgi166 <skskoari@gmail.com>
2017-08-06 16:23:03 +0900

    Remove sample test

--- test/mrb_dir.rb
+++ test/mrb_dir.rb
@@ -2,16 +2,16 @@
 ## dir Test
 ##
 
-assert("dir#hello") do
-  t = dir.new "hello"
-  assert_equal("hello", t.hello)
-end
+# assert("dir#hello") do
+#   t = dir.new "hello"
+#   assert_equal("hello", t.hello)
+# end
 
-assert("dir#bye") do
-  t = dir.new "hello"
-  assert_equal("hello bye", t.bye)
-end
+# assert("dir#bye") do
+#   t = dir.new "hello"
+#   assert_equal("hello bye", t.bye)
+# end
 
-assert("dir.hi") do
-  assert_equal("hi!!", dir.hi)
-end
+# assert("dir.hi") do
+#   assert_equal("hi!!", dir.hi)
+# end
