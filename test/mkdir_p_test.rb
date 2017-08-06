3e5d1fec64fd836ede7cd6cd35886e08193894bf
mgi166 <skskoari@gmail.com>
2017-08-06 16:24:04 +0900

    Add test case

--- test/mkdir_p_test.rb
+++ test/mkdir_p_test.rb
@@ -4,3 +4,20 @@ assert("Dir.mkdir_p when argument given `a`") do
   assert_equal(true, File.directory?("a"))
   Dir.delete("a")
 end
+
+assert("Dir.mkdir_p when argument given `a/b`") do
+  Dir.mkdir_p("a/b")
+  assert_equal(true, File.exist?("a/b"))
+  assert_equal(true, File.directory?("a/b"))
+  Dir.delete("a/b")
+  Dir.delete("a")
+end
+
+assert("Dir.mkdir_p when argument given `a/b/c`") do
+  Dir.mkdir_p("a/b/c")
+  assert_equal(true, File.exist?("a/b/c"))
+  assert_equal(true, File.directory?("a/b/c"))
+  Dir.delete("a/b/c")
+  Dir.delete("a/b")
+  Dir.delete("a")
+end
