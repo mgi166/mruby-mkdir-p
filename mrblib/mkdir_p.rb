9cb834ea211cf014b73948d9a4e7b494fde459ae
mgi166 <skskoari@gmail.com>
2017-08-06 16:23:09 +0900

    Implement `Dir.mkdir_p`

--- mrblib/mkdir_p.rb
+++ mrblib/mkdir_p.rb
@@ -1,4 +1,26 @@
 class Dir
   def self.mkdir_p(dir)
+    descend_path(dir) do |dir|
+      next if File.exist?(dir)
+      Dir.mkdir(dir)
+    end
+  end
+
+  def self.descend_path(path)
+    result = []
+    dirname = path
+
+    while dirname != File::SEPARATOR && dirname != "." do
+      result << dirname
+      dirname = File.dirname(dirname)
+    end
+
+    if block_given?
+      result.reverse.each do |dir|
+        yield dir
+      end
+    else
+      result.reverse
+    end
   end
 end
--- test/mkdir_p_test.rb
+++ test/mkdir_p_test.rb
@@ -1,1 +1,6 @@
-assert
+assert("Dir.mkdir_p when argument given `a`") do
+  Dir.mkdir_p("a")
+  assert_equal(true, File.exist?("a"))
+  assert_equal(true, File.directory?("a"))
+  Dir.delete("a")
+end
